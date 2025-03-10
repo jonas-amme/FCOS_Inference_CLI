715606b0f8a55d8b76467c933bfc4573ac4ddaa6
jonas-amme <ammeling.jonas@gmail.com>
2025-03-10 14:44:32 +0100

    added test case

---
+++ test/test_detect.py
@@ -0,0 +1,59 @@
+import json
+
+from typer.testing import CliRunner
+from ..main import app
+
+from pathlib import Path 
+
+
+runner = CliRunner()
+
+
+def test_detect():
+    """Test the `detect` command for single image inference."""
+
+    # Setup test case 
+    input_path = Path('./test/test.tiff')
+    output_dir = Path('./test')
+    config_path = Path('./configs/FCOS_18.yaml')
+
+    # Run detect command 
+    result = runner.invoke(
+        app,
+        [
+            "detect",
+            str(config_path),
+            str(input_path),
+            str(output_dir),
+        ],
+    )
+
+    # Assert succesful execution
+    assert result.exit_code == 0, f"CLI failed with error: {result.output}"
+
+    # Check that the output directory contains the expected JSON result
+    output_file = Path('./test/test_detections.json')
+    assert output_file.exists(), "No output JSON file was created!"
+
+    # Verify the contents of the JSON file
+    with open(output_file, "r") as f:
+        data = json.load(f)
+        assert "boxes" in data, "Output JSON is missing 'boxes' key!"
+        assert "scores" in data, "Output JSON is missing 'scores' key!"
+        assert "labels" in data, "Output JSON is missing 'labels' key!"
+        assert "image_path" in data, "Output JSON is missing 'image_path' key!"
+        assert data["image_path"] == str(input_path), "Image path in JSON is incorrect!"
+
+
+    # Verify that the predictions are the same
+    test_file = "./test/FCOS_18_test_results.json"
+    test_data = json.load(open(test_file, "r"))
+    pred_data = json.load(open(output_file, "r"))
+
+    for key in ["boxes", "scores", "labels"]:
+        test_values = test_data[key]
+        pred_values = pred_data[key]
+        
+        assert len(test_values) == len(pred_values), f"Different number of values {key} key!"
+        assert all([test_values[i] == pred_values[i] for i in range(len(test_values))]), f"Values are not the same {key} key!"
+
